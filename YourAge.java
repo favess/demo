823821d3a13c22807ab640048612b3ff99f862b4
favess <fav.osarin@gmail.com>
2025-11-06 14:01:09 +0100

    created jave file to ask for age

---
+++ YourAge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class YourAge {
+ 
+    public static void main(String[] args){
+
+        System.out.println("type in your age");
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        System.out.printf("you are %d years old", age);
+        input.close();
+    }
+
+}
